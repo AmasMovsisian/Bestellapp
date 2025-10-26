e7d85ee850b59e17586ae66c94392fa79d395ba8
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-10-26 19:51:13 +0100

    some changes

--- db.js/script.js
+++ db.js/script.js
@@ -5,11 +5,19 @@ function init(){
 function renderDishes() {
     let dishesRef = document.getElementById("dishes");
     for(let i = 0; i < myDishes.length; i++) {
-        dishesRef.innerHTML += `
-        <div class="food_Container${[i]}" id="foodContainer${[i]}">
-            <p><strong>${myDishes[i].name}</strong></p> 
-            <span>${myDishes[i].description}</span> <br><br>
-            <span class="price">${myDishes[i].price.toFixed(2)}€</span> <br><br>
-        </div>`
+        dishesRef.innerHTML += `  
+        <div class="dishes_basket"> 
+        
+            <div class="food_Container${[i]}" id="foodContainer${[i]}">
+                <p><strong>${myDishes[i].name}</strong></p> 
+                <span>${myDishes[i].description}</span> <br><br>
+                <span class="price">${myDishes[i].price.toFixed(2)}€</span> <br><br>
+            </div>
+
+            <div class="add_icon" id="addIcon">
+             + 
+            </div>
+
+        </div> `
     }
 }
