50844ed659b032b7e79947cb2132ae4134d1a633
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-10-30 13:11:10 +0100

    the website is almost ready

--- db.js/script.js
+++ db.js/script.js
@@ -1,6 +1,8 @@
 function init() {
   renderDishes();
   renderBasket();
+  priceCalculation();
+  orderItems();
 }
 
 function renderDishes() {
@@ -28,29 +30,100 @@ function removeFromBasket(index) {
     basketDishes.splice(index, 1);
   }
   renderBasket();
+  priceCalculation();
+  updateOrderButtonStyle();
 }
 
 function clearBasket(index) {
   basketDishes.splice(index, 1);
   renderBasket();
+  priceCalculation();
+  updateOrderButtonStyle();
 }
 
 function addToBasket(i) {
   let dish = myDishes[i];
   let foundIndex = -1;
-
   for (let j = 0; j < basketDishes.length; j++) {
     if (basketDishes[j].name === dish.name) {
       foundIndex = j;
     }
   }
-
   if (foundIndex > -1) {
     basketDishes[foundIndex].amount++;
   } else {
     dish.amount = 1;
     basketDishes.push(dish);
   }
-
   renderBasket();
+  priceCalculation();
+}
+
+function priceCalculation() {
+  let priceCalculationRef = document.getElementById("priceCalculation");
+  let subtotal = 0;
+  for (let i = 0; i < basketDishes.length; i++) {
+    subtotal += basketDishes[i].price * basketDishes[i].amount;
+  }
+  let deliveryCost = 5;
+  let total = subtotal + deliveryCost;
+  if (basketDishes.length === 0) {
+    priceCalculationRef.innerHTML = getHTMLEmptyBasketText();
+  } else {
+    priceCalculationRef.innerHTML = getHTMLForPriceCalculation(
+      subtotal,
+      deliveryCost,
+      total
+    );
+    orderItems();
+  }
+}
+
+function orderItems() {
+  const orderItemsRef = document.getElementById("orderItems");
+  orderItemsRef.innerHTML = `<button id="orderBtn" class="order_BTN" onclick="orderConfirm()">Bestellen</button>`;
+  updateOrderButtonStyle();
+}
+
+function updateOrderButtonStyle() {
+  let orderBtn = document.getElementById("orderBtn");
+  let pricesStyles = document.getElementById("priceCalculation");
+
+  if (orderBtn == null) {
+    return;
+  }
+
+  if (basketDishes.length == 0) {
+    orderBtn.style.opacity = "0.5";
+    orderBtn.style.backgroundColor = "gray";
+    orderBtn.style.color = "black";
+    pricesStyles.style.boxShadow = "none";
+  } else {
+    orderBtn.style.opacity = "1";
+    orderBtn.style.backgroundColor = "";
+    orderBtn.style.color = "";
+    pricesStyles.style.boxShadow = "";
+  }
+}
+
+function orderConfirm() {
+  const orderBtn = document.getElementById("orderBtn");
+
+  if (basketDishes.length > 0) {
+    showDialog();
+  } else {
+    orderBtn.disabled = true;
+  }
+
+  updateOrderButtonStyle();
+}
+
+function showDialog() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("dialog").style.display = "block";
+}
+
+function closeDialog() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("dialog").style.display = "none";
 }
--- db.js/template.js
+++ db.js/template.js
@@ -13,20 +13,38 @@ function getHTMLDishes(i) {
             </div>
 
         </div> `
-}
+};
 
 function getHTMLBasketDishes(j) {
    return `
-            <p><strong>${basketDishes[j].name}</strong></p>
+            <p class="priceCalculationTextes"><strong>${basketDishes[j].name}</strong></p>
             <div class="basket_items">
-
-            <button onclick="removeFromBasket(${j})">-</button> 
+            <button class="pricesCalcBTNs" onclick="removeFromBasket(${j})">-</button> 
             <span>${basketDishes[j].amount}x</span>
-            <button onclick="addToBasket(${j})">+</button> 
+            <button class="pricesCalcBTNs" onclick="addToBasket(${j})">+</button> 
 
             <span>${(basketDishes[j].price * basketDishes[j].amount).toFixed(2)} €</span>
-            <button onclick="clearBasket(${j})"><i class="material-icons">delete</i></button>
+            <button class="remove_all_BTN" onclick="clearBasket(${j})"><i class="material-icons">delete</i></button>
 
             </div>
-        `;
+        `
+};
+
+function getHTMLForPriceCalculation(subtotal, deliveryCost, total) {
+  return `
+    <div class="price_calculation_design">
+        <span class="prices_color">Zwischensumme</span> <span class="prices_color">${subtotal.toFixed(2)} €</span>
+    </div>
+    <div class="price_calculation_design">
+        <span class="prices_color">Lieferkosten</span> <span class="prices_color">${deliveryCost.toFixed(2)} €</span>
+    </div>
+    <div class="price_calculation_design">
+        <span><strong>Gesamt</strong></span> <span><strong>${total.toFixed(2)} €</strong></span>
+    </div>`;
+}
+
+
+function getHTMLEmptyBasketText(){
+  return `<span class="basket_empty_text"><strong>Dein Warenkorb ist noch leer!<br><br>
+  Wähle leckere Pizzen aus und genieße deine Bestellung </strong></span>`
 }
